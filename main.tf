4c347dbb62fcf808fa7f983abbcc8b81ceebf454
ekxtz <ekxtzkriv@gmail.com>
2025-11-27 18:58:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,17 +14,11 @@ provider "google" {
   zone    = "europe-north1-a"
 }
 
-// ===========================================
-// 1. ИСХОДНЫЕ РЕСУРСЫ (СЕТЬ И VM)
-// ===========================================
-
-// 1.1. Сеть (Сохраняем 'instance-vpc-network')
 resource "google_compute_network" "vpc_network" {
   name                    = "instance-vpc-network" 
   auto_create_subnetworks = true
 }
 
-// 1.2. Виртуальные машины (VM) с Nginx
 resource "google_compute_instance" "app_servers" {
   count         = 2 
   
@@ -54,7 +48,6 @@ resource "google_compute_instance" "app_servers" {
   }
 }
 
-// 1.3. Правила Брандмауэра
 resource "google_compute_firewall" "allow_ssh" {
   name    = "allow-ssh-ingress-app"
   network = google_compute_network.vpc_network.name
@@ -75,11 +68,6 @@ resource "google_compute_firewall" "allow_http" {
   source_ranges = ["0.0.0.0/0"] 
 }
 
-// ===========================================
-// 2. ДОБАВЛЕННЫЙ СТЕК HTTP LOAD BALANCER
-// ===========================================
-
-// 2.1. Instance Group (Группирует VM для Backend Service)
 resource "google_compute_instance_group" "instance_group" {
   name    = "web-instance-group"
   zone    = "europe-north1-a"
@@ -100,7 +88,6 @@ resource "google_compute_region_health_check" "http_health_check_modern" {
   }
 }
 
-// 2.3. Backend Service (Сервис бэкенда)
 resource "google_compute_region_backend_service" "web_backend" {
   name        = "web-backend-service"
   protocol    = "HTTP"
@@ -112,21 +99,18 @@ resource "google_compute_region_backend_service" "web_backend" {
   }
 }
 
-// 2.4. URL Map (Карта URL-адресов)
 resource "google_compute_region_url_map" "url_map" {
   name            = "web-url-map"
   region          = "europe-north1"
   default_service = google_compute_region_backend_service.web_backend.self_link
 }
 
-// 2.5. Target HTTP Proxy (Целевой HTTP-прокси)
 resource "google_compute_region_target_http_proxy" "http_proxy" {
   name    = "http-target-proxy"
   region  = "europe-north1"
   url_map = google_compute_region_url_map.url_map.self_link
 }
 
-// 2.6. Forwarding Rule (Правило переадресации - получает внешний IP)
 resource "google_compute_forwarding_rule" "forwarding_rule_modern" {
   name        = "http-forwarding-rule-modern"
   region      = "europe-north1"
@@ -134,17 +118,11 @@ resource "google_compute_forwarding_rule" "forwarding_rule_modern" {
   target      = google_compute_region_target_http_proxy.http_proxy.self_link
 }
 
-// ===========================================
-// 3. ВЫХОДНЫЕ ДАННЫЕ (OUTPUTS)
-// ===========================================
-
-// IP-адрес Load Balancer
 output "load_balancer_ip" {
   description = "The external IP address of the Regional Load Balancer"
   value       = google_compute_forwarding_rule.forwarding_rule_modern.ip_address
 }
 
-// Индивидуальные IP-адреса VM
 output "instance_ips" {
   description = "External IP addresses of the created instances (for debugging)"
   value = google_compute_instance.app_servers[*].network_interface.0.access_config.0.nat_ip
